fdc89f5d8be862bfc6dc341e650ca849c17e27ed
Manuel Salgado <manual.salgado@tambourine.com>
2019-02-11 13:54:08 -0500

    Added components

---
+++ src/components/AstronomyContainer/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import axios from 'axios'
+
+class AstronomyContainer extends React.Component {
+    state = {astronomy: []}
+
+    componentDidMount () {
+        const API_KEY = 'q6OsSmMTqLJIT5cxFFtqf0bZoUCvh9pK10Sfouqu'
+        const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key'
+
+        axios.get(API_KEY+END_POINT)
+                .then(response => {
+                    console.log(response)
+                })
+                .catch(error => {
+                    console.log(error)
+                    
+                })
+    }
+    render () {
+        return (
+            <div />
+        )
+    }
+}
+
+export default AstronomyContainer
